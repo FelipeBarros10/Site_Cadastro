20fd7af35f3f9e33ede87623e01ecd40e0c777fa
FelipeBarros10 <barrosfelipe735@gmail.com>
2025-01-10 20:16:56 -0300

    Creating a general function to insert commands in SQL

--- connect/connectionBd.php
+++ connect/connectionBd.php
@@ -1,32 +1,71 @@
-<?php 
+<?php
 
-  function connectDb (){
-    $hostName = "localhost";
-    $userHost = "root";
-    $password = "";
-    $nameDb = "site_register";
+use function PHPSTORM_META\type;
 
-    $conn = mysqli_connect($hostName, $userHost, $password, $nameDb);
+function connectDb()
+{
+  $hostName = "localhost";
+  $userHost = "root";
+  $password = "";
+  $nameDb = "site_register";
 
-    if($conn){
-      return $conn;
-    } else {
-      echo "erro no banco";
-      return;
-    }
-   
+  $conn = mysqli_connect($hostName, $userHost, $password, $nameDb);
+
+  if ($conn) {
+    return $conn;
+  } else {
+    echo "erro no banco";
+    return;
   }
+}
 
-  function dbQuery ($conn, $query){
-      if($query != ''){
-        $queryResult = mysqli_query($conn, $query);
-        return $queryResult;
-      } else {
-        echo "Erro na query";
-        return;
-    }
+
+
+function dbQueryInsert($conn, $query, array $values)
+{
+
+  if ($query != '') {
+
+    if ($values) {
+
+      $stmt = mysqli_prepare($conn, $query);
+
+      if (isset($stmt)) {
+
+        $typeParam = [];
+
+        foreach ($values as $value) {
+          if (is_string($value)) {
+            $typeParam[] = "s";
+          } else if (is_int($value)) {
+            $typeParam[] = "i";
+          } else {
+            $typeParam[] = "d";
+          }
+        }
+
+        $typeParamToStr = implode($typeParam);
+
+        $bindParam = mysqli_stmt_bind_param($stmt, $typeParamToStr, ...$values);
+
+        if (isset($bindParam)) {
+          $queryResult = mysqli_stmt_execute($stmt);
+          return $queryResult;
+        }
+      }
+    };
+
+  } else {
+    return false;
   }
+}
 
 
-
-?>
+function dbQuerySelect ($conn, $query) {
+  if ($query != '') {
+    $queryResult = mysqli_query($conn, $query);
+    return $queryResult;
+  }  else{
+    return false;
+  }
+}
