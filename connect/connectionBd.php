ab682e6d0f2513d962fca62db943a6e82873d3b5
FelipeBarros10 <barrosfelipe735@gmail.com>
2025-01-22 21:32:13 -0300

    Backend of the action of delete products at table

--- connect/connectionBd.php
+++ connect/connectionBd.php
@@ -20,8 +20,8 @@ function connectDb()
 }
 
 
-function dbQueryInsert($query, $values)
-{
+function dbQuery($query, $values = ""){
+
   $conn = connectDb();
 
   if ($query != '') {
@@ -36,60 +36,6 @@ function dbQueryInsert($query, $values)
 
         if (is_array($values)) {
           foreach ($values as $value) {
-            if (is_string($value)) {
-              $typeParam[] = "s";
-            } else if (is_int($value)) {
-              $typeParam[] = "i";
-            } else {
-              $typeParam[] = "d";
-            }
-          }
-
-          $typeParamToStr = implode($typeParam);
-
-          $bindParam = mysqli_stmt_bind_param($stmt, $typeParamToStr, ...$values);
-        } else {
-          if (is_string($values)) {
-            $typeParam[] = "s";
-          } else if (is_int($values)) {
-            $typeParam[] = "i";
-          } else {
-            $typeParam[] = "d";
-          }
-
-          $typeParamToStr = implode($typeParam);
-
-          $bindParam = mysqli_stmt_bind_param($stmt, $typeParamToStr, $values);
-        }
-
-        if (isset($bindParam)) {
-          $queryResult = mysqli_stmt_execute($stmt);
-          if ($queryResult) {
-            $userId = mysqli_insert_id($conn);
-            return $userId;
-          }
-        }
-      }
-    };
-  } else {
-    return false;
-  }
-}
-
-
-function dbQuerySelect($query, $values = "")
-{
-
-  $conn = connectDb();
-
-  if ($query != '') {
-    if ($values) {
-      $stmt = mysqli_prepare($conn, $query);
-      if (is_array($values)) {
-        $typeParam = [];
-
-        if (isset($stmt)) {
-          foreach ($values as $value) {
             if(filter_var($values, FILTER_VALIDATE_INT)){
               $typeParam[] = "i";
             } elseif (filter_var($values, FILTER_VALIDATE_FLOAT)){
@@ -103,33 +49,36 @@ function dbQuerySelect($query, $values = "")
 
           $bindParam = mysqli_stmt_bind_param($stmt, $typeParamToStr, ...$values);
         } else {
-          return false;
+          if(filter_var($values, FILTER_VALIDATE_INT)){
+            $typeParam[] = "i";
+          } elseif (filter_var($values, FILTER_VALIDATE_FLOAT)){
+            $typeParam[] = "d";
+          } else {
+            $typeParam[] = "s";
+          }
+
+          $typeParamToStr = implode($typeParam);
+
+
+          $bindParam = mysqli_stmt_bind_param($stmt, $typeParamToStr, $values);
         }
-      } else {
 
-        if(filter_var($values, FILTER_VALIDATE_INT)){
-          $typeParam[] = "i";
-        } elseif (filter_var($values, FILTER_VALIDATE_FLOAT)){
-          $typeParam[] = "d";
-        } else {
-          $typeParam[] = "s";
+        if (isset($bindParam)) {
+          $queryResult = mysqli_stmt_execute($stmt);
+
+          if ($queryResult) {
+            if(preg_match("/SELECT/", $query)){
+              $result = mysqli_stmt_get_result($stmt);
+              return $result;
+            } elseif (preg_match("/INSERT/", $query)){
+              $userId = mysqli_insert_id($conn);
+              return $userId;
+            } else {
+              return true;
+            }
+            
+          }
         }
-
-
-        $typeParamToStr = implode($typeParam);
-
-
-        $bindParam = mysqli_stmt_bind_param($stmt, $typeParamToStr, $values);
-      }
-
-      if (isset($bindParam)) {
-        $queryResult = mysqli_stmt_execute($stmt);
-
-        if ($queryResult) {
-          $result = mysqli_stmt_get_result($stmt);
-          return $result;
-        }
-        
       }
     } else {
       $queryResult = mysqli_query($conn, $query);
@@ -139,3 +88,71 @@ function dbQuerySelect($query, $values = "")
     return false;
   }
 }
+
+// $query = "DELETE FROM produtos WHERE ID = ?";
+//   $values = "7";
+//   $deletingTheProduct = dbQuery($query, $values);
+
+
+// function dbQuery($query, $values = "")
+// {
+
+//   $conn = connectDb();
+
+//   if ($query != '') {
+//     if ($values) {
+//       $stmt = mysqli_prepare($conn, $query);
+//       if (is_array($values)) {
+//         $typeParam = [];
+
+//         if (isset($stmt)) {
+//           foreach ($values as $value) {
+//             if(filter_var($values, FILTER_VALIDATE_INT)){
+//               $typeParam[] = "i";
+//             } elseif (filter_var($values, FILTER_VALIDATE_FLOAT)){
+//               $typeParam[] = "d";
+//             } else {
+//               $typeParam[] = "s";
+//             }
+//           }
+
+//           $typeParamToStr = implode($typeParam);
+
+//           $bindParam = mysqli_stmt_bind_param($stmt, $typeParamToStr, ...$values);
+//         } else {
+//           return false;
+//         }
+//       } else {
+
+//         if(filter_var($values, FILTER_VALIDATE_INT)){
+//           $typeParam[] = "i";
+//         } elseif (filter_var($values, FILTER_VALIDATE_FLOAT)){
+//           $typeParam[] = "d";
+//         } else {
+//           $typeParam[] = "s";
+//         }
+
+
+//         $typeParamToStr = implode($typeParam);
+
+
+//         $bindParam = mysqli_stmt_bind_param($stmt, $typeParamToStr, $values);
+//       }
+
+//       if (isset($bindParam)) {
+//         $queryResult = mysqli_stmt_execute($stmt);
+
+//         if ($queryResult) {
+//           $result = mysqli_stmt_get_result($stmt);
+//           return $result;
+//         }
+        
+//       }
+//     } else {
+//       $queryResult = mysqli_query($conn, $query);
+//       return $queryResult;
+//     }
+//   } else {
+//     return false;
+//   }
+// }
